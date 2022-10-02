d37b66f495759baa3f864e02b8831b7f660beaa8
Yubraj Bhandari <yubraj.bhandari.hero@gmail.com>
2022-10-02 22:15:29 +0545

    fixed keywords and authors in text search

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -2,24 +2,41 @@ import React, { useEffect, useState} from 'react'
 
 function SearchItem({item}) {
 
-   
-    // const authors = item.authors.map()
+    
+    let authors = []
+    
+    if(item.authorAffiliations != undefined){
+
+        authors = item.authorAffiliations.map(itm => <li> {itm.meta.author.name}; </li>)
+    }
+    
+    let keywords = []
+
+    if(item.keywords != undefined) {
+    
+        const keys = item.keywords[0].split(" ")
+        console.log(keys)
+    keywords =  keys.map((itm) => (
+            <li className="bg-primary text-white px-3 py-1 rounded-full">
+          {itm}
+        </li>
+      )) ;
+    } 
+    
     return (
         <div className='w-full  p-4 border-2 border-gray-300 rounded-lg'>
             <h1 className='text-xl font-[600] leading-8'>
                 {item.title}
             </h1>
             <ul className='mt-2 text-sm font-[600] text-primary flex space-x-2'>
-                
-                <li>Laura Dennis,</li>
-                <li>Andrea Spaeth,</li>
-                <li>Namni Goel</li>
+            {authors}
             </ul>
             <div className='flex items-center mt-6 text-sm'>
                 Keywords :
                 <ul className='flex space-x-2 ml-4'>
-                    <li className='bg-primary text-white px-3 py-1 rounded-full'>Atmospheric Science</li>
-                    <li className='bg-primary text-white px-3 py-1 rounded-full'>Geography</li>
+                    {keywords}
+                    {/* <li className='bg-primary text-white px-3 py-1 rounded-full'>Atmospheric Science</li>
+                    <li className='bg-primary text-white px-3 py-1 rounded-full'>Geography</li> */}
                 </ul>
             </div>
             <div className='flex justify-between'>
